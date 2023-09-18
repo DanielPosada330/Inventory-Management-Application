64f2ad2fa0713483baff34a9c7fb3840a19de6c8
Daniel Posada <dposa11@wgu.edu>
2023-09-18 07:51:30 -0700

    Create AboutPageController.java Update README.md

---
+++ src/main/java/com/example/demo/controllers/AboutPageController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import com.example.demo.domain.InhousePart;
+import com.example.demo.domain.OutsourcedPart;
+import com.example.demo.domain.Part;
+import com.example.demo.service.OutsourcedPartService;
+import com.example.demo.service.OutsourcedPartServiceImpl;
+import com.example.demo.service.PartService;
+import com.example.demo.service.PartServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+@Controller
+public class AboutPageController {
+    @Autowired
+    private ApplicationContext context;
+
+    @GetMapping("/aboutPage")
+    public String showAboutPage(){
+        return "about";
+
+    }
+
+}
