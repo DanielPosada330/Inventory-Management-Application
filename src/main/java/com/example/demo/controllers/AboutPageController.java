7a1d8a67020870097e4ef3ef0775e3fd030d6b25
Daniel Posada <dposa11@wgu.edu>
2023-09-24 10:25:18 -0500

    Update AboutPageController.java remove redundant code

--- src/main/java/com/example/demo/controllers/AboutPageController.java
+++ src/main/java/com/example/demo/controllers/AboutPageController.java
@@ -1,8 +1,5 @@
 package com.example.demo.controllers;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
